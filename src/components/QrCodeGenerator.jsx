958218ae17cd9902ae3d12ca87e532897da4874e
fsuh <92304761+fsuh@users.noreply.github.com>
2023-07-07 10:23:12 +0300

    complete app

---
+++ src/components/QrCodeGenerator.jsx
@@ -0,0 +1,57 @@
+import { useRef, useState } from "react";
+import QRCode from "react-qr-code";
+import * as htmlToImage from "html-to-image";
+
+const QrCodeGenerator = () => {
+	const [url, setUrl] = useState("");
+	const [qrIsVisible, setQrIsVisible] = useState(false);
+	const qrCodeRef = useRef(null);
+	const downloadQRCode = () => {
+		htmlToImage
+			.toPng(qrCodeRef.current)
+			.then(function (dataUrl) {
+				const link = document.createElement("a");
+				link.href = dataUrl;
+				link.download = "qr-code.png";
+				link.click();
+			})
+			.catch(function (error) {
+				alert("Error generating QR code", error);
+			});
+	};
+	const handleQrCodeGenerator = () => {
+		if (!url) return;
+		setQrIsVisible(true);
+	};
+	return (
+		<div className="qrcode_container">
+			<h1>QR Code Generator</h1>
+			<div
+				className="qrcode_container--parent"
+				ref={qrCodeRef}
+			>
+				<div className="qrcode_input">
+					<input
+						type="text"
+						placeholder="Enter a URL"
+						value={url}
+						onChange={(e) => setUrl(e.target.value)}
+					/>
+					<button onClick={handleQrCodeGenerator}>Generate QR Code</button>
+				</div>
+				{qrIsVisible && (
+					<div className="qrcode_download">
+						<div className="qrcode_image">
+							<QRCode
+								value={url}
+								size={300}
+							/>
+						</div>
+						<button onClick={downloadQRCode}>Download QR Code</button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+export default QrCodeGenerator;
